c631efa950e2257c1e00d7ceaae161014308c427
VasyaSmetankin <medvedevkon70@gmail.com>
2023-07-13 14:32:11 +0300

    Initial Commit

---
+++ GitHubSampleApp/GitHubSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSampleAppApp.swift
+//  GitHubSampleApp
+//
+//  Created by Vasya Smetankin on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
