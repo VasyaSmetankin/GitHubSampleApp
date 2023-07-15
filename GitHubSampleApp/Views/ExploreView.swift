b740846822f5fc232a09f3a0f4b3a089b113b616
VasyaSmetankin <medvedevkon70@gmail.com>
2023-07-15 20:48:09 +0300

    Added Explore View

--- GitHubSampleApp/Views/ExploreView.swift
+++ GitHubSampleApp/Views/ExploreView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct ExploreView: View {
+    
+    @State var str: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    ProfileViewCell(rectangleColor: .purple, rectangleImage: "flame", cellText: "Trending repositories", chevronNumber: 0)
+                        .padding(.leading, -10)
+                        .padding(.trailing, -10)
+                    ProfileViewCell(rectangleColor: .red, rectangleImage: "face.smiling", cellText: "Awesome lists", chevronNumber: 0)
+                        .padding(.leading, -10)
+                        .padding(.trailing, -10)
+                } header: {
+                    Text("Discover")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .textCase(nil)
+                }
+                
+                Section {
+                    RepoCellView(starCount: 5, contributorsCount: 5, userName: "user", RepoName: "Idle")
+                    
+                    
+                    
+                    
+                } header: {
+                    Text("Activity")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .textCase(nil)
+                    
+                }
+                
+               
+            }
+            
+            
+            .navigationTitle("Explore")
+            
+            
+        }
+        .searchable(text: $str)
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
 struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
-        ExploreView()
+        ExploreView(str: "a")
     }
 }
